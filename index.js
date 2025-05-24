f5096c1ee94f86601e37e956bd1949c82c3fd6d0
Thunder200817 <andreashenrique1234@gmail.com>
2025-05-24 17:10:15 -0400

    Deploy

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // filepath: c:\Users\Henry\Documents\Love Bites docs\Love Bites Website\index.js
 const QRCode = require('qrcode');
 
-const url = 'https://yourusername.github.io/your-repo/'; // Replace with your actual URL
+const url = 'https://loveatfirstbiteweb.com/';
 
 QRCode.toFile('website-qr.png', url, {
   color: {
-    dark: '#000',  // QR code color
-    light: '#FFF'  // Background color
+    dark: '#000',
+    light: '#FFF'
   }
 }, function (err) {
   if (err) throw err;
